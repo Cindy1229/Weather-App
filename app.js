8d9b274819ae73c471cfd8325837e30c77360a5c
Cindy1229 <leticiashi3@gmail.com>
2020-06-01 19:39:31 -0400

    commit

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ const iconElement=document.querySelector(".weather-icon");
 const tempElement=document.querySelector(".temperature-value p");
 const descElement=document.querySelector(".temperature-description p");
 const location=document.querySelector(".location p");
+
+const weather={
+  temperature: {
+    value: 18,
+    unit: "celsius"
+  },
+  description: "few clouds",
+  iconId: "01d",
+  city: "London",
+  country: "GB"
+};
+
+function celsiusToFahrenheit(temperature){
+  return (temperature * 9/5)+32;
+}
