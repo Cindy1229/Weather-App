4391cec4d01a88145c43b45baacdf5221b5ed264
Cindy1229 <leticiashi3@gmail.com>
2020-06-01 19:48:32 -0400

    commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const tempElement=document.querySelector(".temperature-value p");
 const descElement=document.querySelector(".temperature-description p");
 const location=document.querySelector(".location p");
 
-const weather={
+/*const weather={
   temperature: {
     value: 18,
     unit: "celsius"
@@ -13,8 +13,23 @@ const weather={
   iconId: "01d",
   city: "London",
   country: "GB"
-};
+};*/
 
 function celsiusToFahrenheit(temperature){
   return (temperature * 9/5)+32;
 }
+
+tempElement.addEventListener("click", function(){
+
+  if(weather.temperature.unit==="celsius"){
+    let fahrenheit=celsiusToFahrenheit(weather.temperature.value);
+    fahrenheit=Math.floor(fahrenheit);
+    tempElement.innerHTML=`${fahrenheit}° <span>F</span>`;
+    weather.temperature.unit="fahrenheit";
+
+  }else{
+    tempElement.innerHTML=`${weather.temperature.value}° <span>C</span>`;
+    weather.temperature.unit="celsius";
+
+  }
+});
