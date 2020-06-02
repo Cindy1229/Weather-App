493558b210b1c305d34543d1062d4867a3d87101
Cindy1229 <leticiashi3@gmail.com>
2020-06-01 20:01:59 -0400

    commit

--- app.js
+++ app.js
@@ -15,12 +15,37 @@ const location=document.querySelector(".location p");
   country: "GB"
 };*/
 
+//First check if the location is available
+if('geolocation' in navigator){
+  navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else {
+  notificationElement.style.display="block";
+  notificationElement.innerHTML="<p>Browser doesn't support Geolocation</p>"";
+}
+
+//Set the current location
+fucntion setPosition(position){
+  let latitude=position.coords.latitude;
+  let longitude=position.coords.longitude;
+
+  //Get the weather info from the API
+  getWeather(latitude, longitude);
+}
+
+//Show the error
+function showError(error){
+  notificationElement.style.display="block";
+  notificationElement.innerHTML=`<p> ${error.message} </p>`;
+}
+
+//Convert C to F
 function celsiusToFahrenheit(temperature){
   return (temperature * 9/5)+32;
 }
 
 tempElement.addEventListener("click", function(){
 
+  if(weather.temperature===undefined) return;
   if(weather.temperature.unit==="celsius"){
     let fahrenheit=celsiusToFahrenheit(weather.temperature.value);
     fahrenheit=Math.floor(fahrenheit);
@@ -30,6 +55,5 @@ tempElement.addEventListener("click", function(){
   }else{
     tempElement.innerHTML=`${weather.temperature.value}° <span>C</span>`;
     weather.temperature.unit="celsius";
-
   }
 });
